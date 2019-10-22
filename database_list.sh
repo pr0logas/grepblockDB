396fc8cfa99cd08a4ebb09bb59f8f84369b94288
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-10-23 01:01:57 +0300

    Horizen

--- database_list.sh
+++ database_list.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-database=( 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin'
+database=( 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin' 'horizen'
 )
