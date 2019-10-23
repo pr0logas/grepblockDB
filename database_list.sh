5c9c25a225d10689914aad27b7e2f474cee34ca9
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-10-23 18:38:11 +0300

    qq

--- database_list.sh
+++ database_list.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-database=( 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin' 'horizen'
-)
+database=( 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin' 'horizen')
