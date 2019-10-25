6e5310b6ac8e0f06cff873b02d586d580ea86e02
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-10-25 23:20:24 +0300

    Check for invalid characters

--- execute.sh
+++ execute.sh
@@ -21,6 +21,9 @@ file="${path1}${path2}"
 # Check if lua base64 path arrived?
 if [[ $(echo $path2) ]]; then
         echo "ALL good let's continue" > /dev/null
+elif [[ $1 =~ ^[`!@#$%^&*()_+:\"\',./\\]+$ ]]; then
+	echo "`date +%Y-%m-%d\|%H:%M:%S\|%N` FATAL error - invalid characters" >> $logFile
+	exit 1
 else
         echo "`date +%Y-%m-%d\|%H:%M:%S\|%N` FATAL error - there are no base64 path from NGINX LUA module?" >> $logFile
         exit 1
