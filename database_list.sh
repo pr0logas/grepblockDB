102d5432370842903f88c9fbe3331d867c68ec31
pr0logas <prologas@protonmail.com>
2019-12-03 23:10:10 +0200

    Bitcoin-cash

--- database_list.sh
+++ database_list.sh
@@ -3,5 +3,5 @@
 #database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin' 'solaris' 'zcash' 'decred' 'bitcoin-gold' 'digibyte'
 #)
 
-database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris'
+database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris' 'bitcoin-cash'
 )
--- execute.sh
+++ execute.sh
@@ -35,7 +35,7 @@ fi
 ### Preparing to search content in MongoDB ###
 
 # Assets to loop
-database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris'
+database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris' 'bitcoin-cash'
 )
 
 function checkAssetNameAndTicker() {
