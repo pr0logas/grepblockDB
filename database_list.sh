8f250e55173fdadb0fda7557fa6600135649546c
CICDevelopment <development@adeptio.cc>
2019-12-06 18:27:37 +0200

    Structure

--- database_list.sh
+++ database_list.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-#database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'bitcoin-cash' 'ravencoin' 'solaris' 'zcash' 'decred' 'bitcoin-gold' 'digibyte'
-#)
-
-database=( 'horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris' 'bitcoin-cash' 'bitcoin-gold' 'digibyte'
-)
+database=('horizen' 'dash' 'polis' 'adeptio' 'pivx' 'bitcoin' 'snowgem' 'zcoin' 'syscoin' 'litecoin' 'ravencoin' 'decred' 'solaris' 'bitcoin-cash' 'bitcoin-gold' 'digibyte')
